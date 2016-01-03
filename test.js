c53551966b2dd3b620fc89b19752ec205802decd
pemrouz <p3pedram@hotmail.com>
2016-01-03 21:29:44 +0000

    graceful return

--- test.js
+++ test.js
@@ -2,7 +2,13 @@ var expect = require('chai').expect
   , last = require('./')
 
 describe('last', function() {
+
   it('should return last element', function() {
     expect(last(['a','b'])).to.equal('b')
   })
+
+  it('should return last element', function() {
+    expect(last()).to.equal(undefined)
+  })
+
 })
